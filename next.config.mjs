70692e991072c85b74279ece7dd2c81d545d4c4a
Sandeep Pande <pandesandeep88@gmail.com>
2024-12-12 17:44:33 +0530

    changes to headers and SEO

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/',
+            headers: [
+              {
+                key: 'x-custom-header',
+                value: 'my custom header value',
+              },
+              {
+                key: 'x-another-custom-header',
+                value: 'my other custom header value',
+              },
+              {
+                key: 'x-another-custom-header',
+                value: 'my other custom header value',
+              },
+              {
+                key: 'netlify-vary',
+                value: 'query'
+              }
+            ],
+          },
+        ]
+      },
 };
 
 export default nextConfig;
